0d098cd383018950acf57424b97183ca8b4fb0f6
김태형 <91590335+rlaxo3814@users.noreply.github.com>
2021-12-04 23:53:47 +0900

    SplitNumber

---
+++ SplitNumber.java
@@ -0,0 +1,22 @@
+package mc.sn.day2;
+
+public class Opmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int number =123456;//653148,987456,528963,547512
+		int sum = 0;
+		for(int i=0;i<6;i++) {
+			int splitnumber = number % 10;
+			//System.out.println(splitnumber);
+			number = number / 10;
+			//System.out.println(number);
+			sum = sum + splitnumber;
+			System.out.println(sum);
+		}
+		System.out.println();
+		System.out.println(sum);
+	}
+
+}
