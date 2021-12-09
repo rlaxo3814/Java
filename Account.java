882f81ee10f131abd861f6b3aa19b6aa55bfbc91
김태형 <91590335+rlaxo3814@users.noreply.github.com>
2021-12-09 19:38:49 +0900

    Account Pratice1

---
+++ Account.java
@@ -0,0 +1,21 @@
+package mc.sn.day6;
+
+public class Account {
+	private int balance;
+	public static final int MIN_VALUE = 0;
+	public static final int MAX_VALUE = 1000000;
+	private String ano;
+	private String owner;
+	
+	 
+	 
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+}
