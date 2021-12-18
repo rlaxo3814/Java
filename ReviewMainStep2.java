0a173deb25d5d823001c7d359ffd9c904c802460
김태형 <91590335+rlaxo3814@users.noreply.github.com>
2021-12-18 23:06:17 +0900

    ReviewMainStep2+BasicStatement

---
+++ ReviewMainStep2.java
@@ -0,0 +1,15 @@
+package mc.sn.step2;
+
+public class ReviewMainStep2 {
+	
+	public static void main(String[] args) {
+	// 외부 서비스클래스 BasicStatement의 인스턴스를 생성하시오
+		BasicStatement basic =  new BasicStatement();
+		//basic.printTwo();
+		//basic.printDan(4);
+		//basic.printAllDan();
+		basic.printNumber(1, 10);
+		
+		// BasicStatement 인스턴스가 가지고 있는 모든 메소드를 알맞게 사용하여 결과를 출력하시오
+	}
+}
